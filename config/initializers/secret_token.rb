cb8c45b4d897a02433bbf61c96e933d39297689a
Nathan Jones <thenathanjones@gmail.com>
2012-07-28 16:04:49 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Madrid::Application.config.secret_token = '21a1c13b1f35206d322b4056f1e846f8fb075f2c7a2ad2437cad8b0a62efff7cb7ef6b79df4142e2bd8e2b87a9acabd4d97ffd9092ec1f48521bcf51ebd3284b'
